7351cf12cfaf098f2e5f08bd6a44486d05ad9ac1
Olivia Cubela <oliviacubela@gmail.com>
2020-09-28 00:03:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,34 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirates = Pirate.new(params[:pirates])
+
+      params[:pirates][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ships.all
+
+      erb :'pirates/show'
+    end
+    #
+    # get '/pirates/:id' do
+    #   @pirates = Pirates.all
+    #   binding.pry
+    # end
+    #
+    # post '/pirates' do
+    #   erb :show
+    # end
 
   end
 end
