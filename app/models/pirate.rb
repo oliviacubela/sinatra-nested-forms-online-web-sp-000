b82dd5d402607e4771c7ea09f43b217ea619f614
Olivia Cubela <oliviacubela@gmail.com>
2020-09-28 00:26:17 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -3,10 +3,10 @@ class Pirate
   attr_reader :name, :weight, :height
   @@pirates = []
 
-  def initialize(args)
-    @name = args[:name]
-    @weight = args[:weight]
-    @height = args[:height]
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
     @@pirates << self
   end
 
